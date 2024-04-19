63d9ade63324290989189a71311738c80a4850a1
rokonmahmud <rokonudden6969@gmail.com>
2024-04-19 16:38:58 +0600

    add error element component

--- src/assets/ErrorElement.jsx
+++ src/assets/ErrorElement.jsx
@@ -6,7 +6,7 @@ const ErrorElement = () => {
         <div className="bg-[#FCF9ED] flex justify-center flex-col items-center">
             <img className=" mx-auto" src="https://i.ibb.co/kqLM214/error-element.png" alt="" />
             <Link to={'/'}>
-            <button  className="text-xl mb-8 w-32 h-16 bg-[#FF3811] hover:scale-95 text-white duration-300">Go Back</button>
+            {/* <button  className="text-xl mb-8 w-32 h-16 bg-[#FF3811] hover:scale-95 text-white duration-300">Go Back</button> */}
             </Link>
         </div>
 
